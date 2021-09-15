19dd7ec7258c0c36409e9a877e9e6faf541b5fbb
Junghoon-P <qkr0454@naver.com>
2021-09-16 01:40:22 +0900

    change redux

---
+++ src/Components/CartNoti.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import CartIcon from "./CartIcon";
+
+export default function CartNoti() {
+  const items = useSelector((store) => store.cartReducer);
+  const history = useHistory();
+
+  return (
+    <Icon onClick={() => history.push("/cart")}>
+      <ItemCount>
+        <span>{items.length}</span>
+      </ItemCount>
+      <CartIcon width="32" height="32" />
+    </Icon>
+  );
+}
+
+const Icon = styled.div`
+  position: relative;
+`;
+
+const ItemCount = styled.div`
+  position: absolute;
+  top: -7px;
+  right: -7px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 19px;
+  height: 20px;
+  border-radius: 2px;
+  background-color: #e82c23;
+  font-size: 11px;
+`;
